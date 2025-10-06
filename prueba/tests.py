2d518998ec61d2b841509429124adadbd77a4608
Wasb3er <cam.alchao@duocuc.cl>
2025-10-05 22:56:19 -0300

    Agrego template de menú y nuevas vistas en Django

---
+++ prueba/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class IndexViewTest(TestCase):
+    def test_index_page_status_code(self):
+        response = self.client.get(reverse('index'))  # nombre de la vista en urls.py
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page_contains_text(self):
+        response = self.client.get(reverse('index'))
+        self.assertContains(response, "Ordena Aquí")  # verifica que aparezca ese texto
